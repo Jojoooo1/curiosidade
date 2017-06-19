5a0d7194c62ae0f445356753d794f846e670d722
jonathan chevalier <chevalier.jonathan@live.fr>
2017-06-19 20:52:15 -0300

    added config file for figaro

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,8 +12,8 @@ set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rben
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 set :rbenv_roles, :all # default value
 
-# set :linked_files, %w{config/database.yml config/application.yml config/secrets.yml}
-set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/application.yml', 'config/secrets.yml')
+set :linked_files, %w{config/database.yml config/application.yml config/secrets.yml}
+# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/application.yml', 'config/secrets.yml')
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
